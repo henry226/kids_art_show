a29ea20b59131579fd3a69fc19bebd92d6c2860f
henry226 <henry5581984@gmail.com>
2018-10-28 12:53:24 -0400

    added home and about routes

--- Software_Enginner_Semester_Project/kids_art_show/views.py
+++ Software_Enginner_Semester_Project/kids_art_show/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import views
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Page</h1>')
